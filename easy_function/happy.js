a78f5c2b767f13560a703c548826c77607f5d2f3
alaosama <alahyaneosama1@gmail.com>
2023-11-07 19:33:51 +0100

    Commit first function

---
+++ easy_function/happy.js
@@ -0,0 +1,30 @@
+// function = A section of reusable code.
+
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function add(x, y) {
+    return x * y;
+}
+
+function add(x, y) {
+    return x / y;
+}
+
+function isEven(number) {
+    return number % 2 === 0 ? true : false;
+}
+
+function isValidEmail(email) {
+    return email.includes("@") ? true : false;
+}
+
+console.log(isValidEmail("osama@fake.com"));
+console.log(isValidEmail("osama.com"));
+console.log(isValidEmail("osama@fake.com"));
