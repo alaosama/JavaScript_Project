f59a68815194529cbfc8ac6b28db452e34ba3d72
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2023-11-28 22:07:07 +0100

    Update js.js

--- Maze_(2)/js.js
+++ Maze_(2)/js.js
@@ -1,1 +1,19 @@
+const mazeElement = document.getElementById("maze");
+const ballElemnt =  [];
+
+const pathW = 25; // Path width
+const wallW =10;  // Wall width
+
+const balls = [
+    { column: 0, row: 0 },
+    { column: 9, row: 0 },
+    { column: 0, row: 8 },
+    { column: 9, row: 8 },
+
+].map((ball) => ({
+    x: ball.column * (wallW + pathW) + (wallW / 2 + pathW / 2),
+    y: ball.row * (wallW + pathW) + (wallW / 2 + pathW / 2),
+    velocityX: 0,
+    velocityY: 0,
+}));
 
