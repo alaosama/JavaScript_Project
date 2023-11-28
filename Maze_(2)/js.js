196cacb8ee37007331667f6066de1d28401af9b1
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2023-11-28 22:07:57 +0100

    Update js.js

--- Maze_(2)/js.js
+++ Maze_(2)/js.js
@@ -17,3 +17,37 @@ const balls = [
     velocityY: 0,
 }));
 
+balls.forEach(({ x, y}) => {
+    const ball = document.createElement("div");
+    ball.setAttribute("class", "ball");
+    ball.style.cssText = `left: ${x}px; top: ${y}px; `;
+    mazeElement.appendChild(ball);
+    ballElemnts.push({ ball, speedX, speedY });
+
+});
+
+
+const walls = [
+    { column: 0, row: 0, horizontal: true, length: 10 },
+    { column: 0, row: 0, horizontal: false, length: 9 },
+    { column: 0, row: 9, horizontal: true, length: 10 },
+    { column: 10, row: 0, horizontal: false, length: 9 },
+].map((wall) => ({
+    x: wall.column * (pathW + wallW),
+    y: wall.row * (pathW + wallW),
+    horizontal: wall.horizontal,
+    length: wall.length * (pathW + wallW),
+}));
+
+walls.forEach(({ x, y, horizontal, length}) => {
+    const wamm = document.createElement("div");
+    wall.setAttribute("class", "wall");
+    wall.style.cssText = `
+        left: ${x}px;
+        top: ${y}px;
+        width: ${wallW}px
+        height: ${length}px
+        transform: rolate(${horizontal ? -90 : 0}deg);`;
+
+    mazeElement.appendChild(wall);
+});
