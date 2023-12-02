1f54a7d8970b22ec40a6c4dd2f2ac725529fb57d
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2023-12-02 23:53:32 +0100

    Update js.js
    Add audio sun

--- My-Game/Virtual_Piano/js.js
+++ My-Game/Virtual_Piano/js.js
@@ -1,5 +1,12 @@
 const pianokeys = document.querySelector(".piano-keys .key");
 
+
+let audio = new Audio("");
+
+const playTune = (key) => {
+    audio.play();
+}
+
 pianokeys.forEach(key => {
     key.add
 });
