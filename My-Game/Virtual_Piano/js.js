352e190462ad58c47fb216776023ccfda7f48c7d
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2023-12-03 11:39:56 +0100

    Update js.js
    Add playTune variable

--- My-Game/Virtual_Piano/js.js
+++ My-Game/Virtual_Piano/js.js
@@ -4,8 +4,15 @@ const pianokeys = document.querySelector(".piano-keys .key");
 let audio = new Audio("");
 
 const playTune = (key) => {
+    audio.src = `${key}`;
     audio.play();
-}
+
+    const clickedkey = document.querySelector(`[data-key="${key}"]`);
+    clickedkey.classList.add("active");
+    setTimeout(() => {
+        clickedkey.classList.remove("active");
+    }, 150);
+};
 
 pianokeys.forEach(key => {
     key.add
