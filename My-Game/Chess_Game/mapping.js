47073e9d588d4fe44904852bbe4de53198677275
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2023-12-04 20:51:03 +0100

    Update mapping.js
    Add Code mapping js

--- My-Game/Chess_Game/mapping.js
+++ My-Game/Chess_Game/mapping.js
@@ -1,1 +1,19 @@
 
+
+const arrayOfFiles = document.querySelector(".square");
+
+const filesNameArray = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+for(const i of arrayOfFiles) {
+    let counter = 1;
+    for (const el of i.children) {
+        console.log(el.setAttribute("id", filesNameArray[fileNumber] + counter));
+        //console.log(el);
+        counter++;
+    }
+    fileNumber++;
+}
+
+for (const i of arrayOfFiles) {
+    // console.log(i.children);
+}
