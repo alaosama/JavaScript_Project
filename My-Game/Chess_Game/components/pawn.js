b6f2b1bce786dcdbe376294ea1c7dfe6c40817c6
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2023-12-05 20:08:59 +0100

    Update pawn.js
    Working on pawn

--- My-Game/Chess_Game/components/pawn.js
+++ My-Game/Chess_Game/components/pawn.js
@@ -1,1 +1,9 @@
+// create pawn component
+const pawnCompo = "<div class='pawnCompo'><img src='./images/pieces/black/pawn.png' alt='chess-pawn-piece image'></div>";
 
+
+filesNameArray.forEach((i) => {
+    const id = i + 2;
+    const element = document.getElementById(id);
+    element.innerHTML = pawnCompo;
+});
