87c465c74dd09038e112177b9f6652d477ff1849
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2023-12-05 19:59:29 +0100

    Update click.js

--- My-Game/Chess_Game/click.js
+++ My-Game/Chess_Game/click.js
@@ -4,5 +4,10 @@ for (const x of allSquares) {
     x.addEventListener("click", function () {
         const idOfElement = x.getAttribute("id");
         //console.log(idOfElement);
+        if( innerHtmlOfElement.includes("black") || innerHtmlOfElement.includes("white"))
+        {
+            document.getElementById(idOfElement).style.backgroundColor = 'yellow'
+            
+        }
     });
 }
