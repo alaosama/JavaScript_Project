4ca172e55e96e52214cc7fdf2357a1bf255b3158
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2023-12-06 12:17:00 +0100

    Update click.js
    Add modifications

--- My-Game/Chess_Game/click.js
+++ My-Game/Chess_Game/click.js
@@ -2,12 +2,34 @@ const allSquares = document.getElementById("square");
 
 for (const x of allSquares) {
     x.addEventListener("click", function () {
-        const idOfElement = x.getAttribute("id");
-        //console.log(idOfElement);
-        if( innerHtmlOfElement.includes("black") || innerHtmlOfElement.includes("white"))
-        {
-            document.getElementById(idOfElement).style.backgroundColor = 'yellow'
+        // const idOfElement = x.getAttribute("id");
+        // //console.log(idOfElement);
+        // const innerHtmlOfElement = document.getElementById(idOfElement).innerHTML;
+
+        // if( innerHtmlOfElement.includes("black") || innerHtmlOfElement.includes("white"))
+        // {
+        //     document.getElementById(idOfElement).style.backgroundColor = 'yellow'
             
+        // }
+
+        // push element in empty Array
+        clickeElments.push(x);
+
+        //console.log(clickeElements);
+
+        if(clickeElments.length > 0) {
+
+        }
+
+        const idOfElement = x.getAttribute("id");
+        const innerHtmlOfElement = document.getElementById(idOfElement).innerHTML;
+
+        if ( clickeElments.length > 0)
+
+        if (
+            innerHtmlOfElement.includes("black") || innerHtmlOfElement.includes("white")
+        ) {
+
         }
     });
 }
