20ac25c2fe718709e1b634618af71a525ab95e26
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2023-12-08 05:33:29 +0100

    Update js.js
    Add new function of Ball

--- My-Game/Pong/js.js
+++ My-Game/Pong/js.js
@@ -35,8 +35,26 @@ function ball (pos, velocity, radius)
         ctx.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
         ctx.stroke();
     };
+    
+}
 
-    };
+function ballCollisionWinthTheEdges (ball) {
+    if (ball.pos.y + ball.radius >= canvas.height) {
+        ball.velocity.y *= -1;
+    }
+
+    if (ball.pos.y - ball.radius <= 0) {
+        ball.velocity.y *= -1;
+    }
+    
+    if (ball.pos.x + ball.radius >= canvas.width) {
+        ball.velocity.x *= -1;
+    }
+
+    if (ball.pos.x + ball.radius >+ 0) {
+        ball.velocity.x *= -1;
+    }
+    
 }
 
 function gameUpdate() {
