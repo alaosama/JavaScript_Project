157530e21d973ff8df1c0c689aebed576c360cd4
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2023-12-13 21:13:21 +0100

    Update js.js
    Add all const for game

--- Rack_Paper_Scissors/js.js
+++ Rack_Paper_Scissors/js.js
@@ -1,74 +1,74 @@
-const gameContainer = document.querySelector(".container"),
-    userResult = document.querySelector(".user-result img"),
-    cpuResult = document.querySelector(".user-result img"),
-    result = document.querySelector(".result"),
-    optionImages = document.querySelectorAll(".option-img");
+// const gameContainer = document.querySelector(".container"),
+//     userResult = document.querySelector(".user-result img"),
+//     cpuResult = document.querySelector(".user-result img"),
+//     result = document.querySelector(".result"),
+//     optionImages = document.querySelectorAll(".option-img");
 
-// Loop through each option image element
+// // Loop through each option image element
 
-optionImages.forEach((image, index) => {
-    image.addEventListener("click", (e) => {
-        image.classList.add("active");
+// optionImages.forEach((image, index) => {
+//     image.addEventListener("click", (e) => {
+//         image.classList.add("active");
 
-        userResult.src = cpuResult.src = 
-            ".png";
-        result.textContent = 
-            ""
+//         userResult.src = cpuResult.src = 
+//             ".png";
+//         result.textContent = 
+//             ""
     
-        // Loop through each option image again
+//         // Loop through each option image again
 
-        optionImages.forEach((image2, index2) => {
-            index != index2 && image2.classList.remove("active");
-        });
+//         optionImages.forEach((image2, index2) => {
+//             index != index2 && image2.classList.remove("active");
+//         });
 
-        gameContainer.classList.add("start");
+//         gameContainer.classList.add("start");
 
-        // Set a timeout to delay the result calculation
+//         // Set a timeout to delay the result calculation
 
-        let time = setTimeout(() => {
-            gameContainer.classList.remove("start");
+//         let time = setTimeout(() => {
+//             gameContainer.classList.remove("start");
 
-            // Get the source of the clicked option image 
-            let imageSrc = e.target.querySelector("img").src;
+//             // Get the source of the clicked option image 
+//             let imageSrc = e.target.querySelector("img").src;
             
-            // Set the user image to the clicked option image
+//             // Set the user image to the clicked option image
 
-            userResult.src = imageSrc;
+//             userResult.src = imageSrc;
         
-            // Generate a random number berween 0 and 2
+//             // Generate a random number berween 0 and 2
 
-            let randomNumber = Math.floor(Math.random() * 3);
+//             let randomNumber = Math.floor(Math.random() * 3);
 
-            // Creat an array of CPU image option
+//             // Creat an array of CPU image option
 
-            let cpuImages = [
-                "",
-                "",
-                ""
-            ];
+//             let cpuImages = [
+//                 "",
+//                 "",
+//                 ""
+//             ];
 
-            // Set the CPU image to a random option from the array
+//             // Set the CPU image to a random option from the array
 
-            cpuResult.src = cpuImages[randomNumber];
+//             cpuResult.src = cpuImages[randomNumber];
 
-            // Assign a letter value to the CPU option (R for rock, P for paper, S for scissors)
-            let cpuValue = ["R", "P", "S"][index];
+//             // Assign a letter value to the CPU option (R for rock, P for paper, S for scissors)
+//             let cpuValue = ["R", "P", "S"][index];
             
-            // Create an object with all possible outcomes
+//             // Create an object with all possible outcomes
 
-            let outcomes = {
-                RR: "Draw", RP: "Cpu", RS: "User", PP: "Draw", PR: "User", PS: "Cpu", SS: "Draw", SR: "Cpu", SP: "User"
-            };
+//             let outcomes = {
+//                 RR: "Draw", RP: "Cpu", RS: "User", PP: "Draw", PR: "User", PS: "Cpu", SS: "Draw", SR: "Cpu", SP: "User"
+//             };
 
-            // Look up the outcome value based on user and CPU options
+//             // Look up the outcome value based on user and CPU options
 
-            let outcomeValue = outcomes[userValue + cpuValue];
+//             let outcomeValue = outcomes[userValue + cpuValue];
 
-            // Display the result
-            result.textContent = userValue === cpuValue ? "Match Draw" : `${outcomeValue} Won!`;
-        }, 2500);
-    });
-});
+//             // Display the result
+//             result.textContent = userValue === cpuValue ? "Match Draw" : `${outcomeValue} Won!`;
+//         }, 2500);
+//     });
+// });
 
 
 
@@ -127,3 +127,16 @@ optionImages.forEach((image, index) => {
 //     }
 //     resultDisplay.innerHTML=result
 // }
+
+
+// DOM
+const btnRules = document.querySelector('.rules-btn');
+const btnClose = document.querySelector('.close-btn');
+const modalRules = document.querySelector('.modal');
+
+btnClose.addEventListener('click', () => {
+    modalRules.classList.toggle('show-modal')
+})
+btnClose.addEventListener('click', () => {
+    modalRules.classList.toggle('show-modal')
+})
