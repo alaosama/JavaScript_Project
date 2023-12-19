36997bf0bca9f0551edbcd2e92cf1f045007e163
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2023-12-19 21:37:29 +0100

    Update js.js
    Add js code for my Game

--- My-Game/Memory_Game/js.js
+++ My-Game/Memory_Game/js.js
@@ -5,4 +5,19 @@ let shuf_emojs = emojs.sort(() => (Math.random() > .5) ? 2 : -1);
 for ( let i=0; i < emojs.length; i++) {
     let box = document.createElement('div')
     box.className = 'item';
+    box.innerHTML = shuf_emojs[i];
+
+    box.onclick = function() {
+        this.classList.add('boxOpen')
+        setTimeout(function() {
+            if(document.querySelector('.boxOpen').length > 1) {
+                if(document.querySelectorAll('.boxOpen')[0].innerHTML == document.querySelector('.boxOpen')[1],innerHTML){
+                    document.querySelector('.boxOpen')[0],classList.add('boxMatch')
+                }
+            }
+        },500)
+    }
+
+    document.querySelector('.game').appendChild(box);
+
 }
