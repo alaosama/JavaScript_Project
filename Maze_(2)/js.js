ca575e5dd4ca8be20e8a99b1e065bc387e11de08
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2023-12-29 22:54:21 +0100

    Update js.js
    Add DrawMaze function
    DrawMap function has been modified

--- Maze_(2)/js.js
+++ Maze_(2)/js.js
@@ -44,3 +44,86 @@ function toggleVisablity(id) {
         document.getElementById(id).style.visibility = "visible";
     }
 }
+
+
+
+
+
+function DrawMaze(Maze, ctx, endSprite = null) {
+    var map = Maze.map();
+    var cellSize = cellsize;
+    var drawEndMethod;
+    ctx.lineWidth = cellSize / 40;
+
+    this.redrawMaze = function(size) {
+        cellSize = sier;
+        ctx.lineWidth = cellSize / 50;
+        DrawMap(),
+        drawEndMethod();
+    };
+
+    function drawCell(xCord, yCord, cell) {
+        var x =xCord * cellSize;
+        var y = yCord * cellSize;
+
+        if (cell.n == false) {
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+            ctx.lineTo(x + cellSize, y);
+            ctx.stroke();
+        }
+        if (cell.s == false) {
+            ctx.beginPath();
+            ctx.moveTo(x, y + cellSize);
+            ctx.lineTo(x + cellSize, y + cellSize);
+            ctx.stroke();
+        }
+        if (cell.e == false) {
+            ctx.beginPath();
+            ctx.moveTo(x + cellSize, y);
+            ctx.lineTo(x + cellSize, y + cellSize);
+            ctx.stroke();
+        }
+        if (cell.w == false) {
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+            ctx.lineTo(x, y + cellSize);
+            ctx.stroke();
+        }
+    }
+
+    function DrawMap () {
+        for (x = 0; x < map.length; x++) {
+            for (y = 0; y < map[x].length; y++) {
+                drawCell(x, y, map[x][y]);
+            }
+        }
+    }
+
+    function drawEndFlag() {
+        var coord = Maze.endCoord();
+        var gridSize = 4;
+        var fraction = cellSize / gridSize - 2;
+        var colorSwap = true;
+        for (let y = 0; y < gridSize; y++) {
+            if (gridSize % 2 == 0) {
+                colorSwap = !colorSwap;
+            }
+
+            for (let x =0; x < gridSize; x++) {
+                ctx.beginPath();
+                ctx.rect (coord.x * cellSize + x * fraction + 4.5,
+                        coord.y * cellSize + y * fraction + 4.5,
+                        fraction,
+                        fraction
+                    );
+                    if (colorSwap) {
+                        ctx.fillStyle = "rgba(0, 0, 0.8)";
+                    } else 
+                    ctx.fillStyle = "rgba(255, 255, 244, 0.8)"; 
+                }
+            ctx.fill ();
+            colorSwap = !colorSwap;
+        }
+    }
+}
