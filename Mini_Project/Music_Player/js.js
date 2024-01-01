c70940821dbff95993afaaf0196a6c3953b5d487
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2024-01-01 13:58:18 +0100

    Update js.js
    Add elements seem to be related to an audio player interface.

---
+++ Mini_Project/Music_Player/js.js
@@ -0,0 +1,9 @@
+const audio = document.querySelector('#audioPlayer');
+const playPauseButton = document.querySelector('#play-pause-button');
+const prevButton = document.querySelector('#prevButton');
+const nextButton = document.querySelector('#nextButton');
+const songTitle = document.querySelector('#songTitle');
+const songArtist = document.querySelector('#songArtist');
+const songImg = docu.querySelector('songImg');
+
+const backgroundBlor = document.querySelector('.backgroundBlur');
