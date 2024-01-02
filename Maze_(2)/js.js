6a27b9b8a2c995c650c1cad56274ff7c45819a05
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2024-01-02 16:21:49 +0100

    Update js.js
    Add drawCell function to the first function

--- Maze_(2)/js.js
+++ Maze_(2)/js.js
@@ -239,6 +239,36 @@ function DrawMaze(Maze, ctx, endSprite = null) {
         DrawMap(),
         drawEndMethod();
     };
+    
+    function drawCell(xCord, yCord, cell) {
+        var x =xCord * cellSize;
+        var y = yCord * cellSize;
+
+        if (cell.n == false) {
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+            ctx.lineTo(x + cellSize, y);
+            ctx.stroke();
+        }
+        if (cell.s == false) {
+            ctx.beginPath();
+            ctx.moveTo(x, y + cellSize);
+            ctx.lineTo(x + cellSize, y + cellSize);
+            ctx.stroke();
+        }
+        if (cell.e == false) {
+            ctx.beginPath();
+            ctx.moveTo(x + cellSize, y);
+            ctx.lineTo(x + cellSize, y + cellSize);
+            ctx.stroke();
+        }
+        if (cell.w == false) {
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+            ctx.lineTo(x, y + cellSize);
+            ctx.stroke();
+        }
+    }
 
 
 function drawEndSprite() {
