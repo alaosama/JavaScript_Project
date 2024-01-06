7cfb27275c20cb00d50b4ea8b43815b729452a27
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2024-01-06 08:11:01 +0100

    Update js.js
    Add Setting Game Options

--- My-Game/Word_Guess/js.js
+++ My-Game/Word_Guess/js.js
@@ -1,5 +1,7 @@
-// Setting Game Name
 let gameName = "Guess The Word";
 document.querySelector("h1").innerHTML = gameName;
 document.querySelector("footer").innerHTML = `${gameName} Game Created By Elzero Web School`;
 
+let numberOfTries = 6;
+let numberOfLetters = 6;
+let currentTry = 1;
