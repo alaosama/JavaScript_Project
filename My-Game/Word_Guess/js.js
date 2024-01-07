2279fa6fb4332003e7072870897220187c15181e
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2024-01-07 22:32:15 +0100

    Update js.js
    Add Create Main Try Div And Create Inputes

--- My-Game/Word_Guess/js.js
+++ My-Game/Word_Guess/js.js
@@ -7,7 +7,32 @@ let numberOfLetters = 6;
 let currentTry = 1;
 
 
+function generateInput() {
+    const inputsContainer = document.querySelector(".inputs");
 
+    // Create Main Try Div
+    for (let i = 1; i <= numberOfTries; i++) {
+        const tryDiv = document.createElement("div");
+        tryDiv.classList.add(`try-${i}`);
+        tryDiv.innerHTML = `<span>Try ${i}</span>`;
+
+        if (i !== 1) tryDiv.classList.add("disabled-inputs");
+
+        // Create Inputes
+        for (let j = 1; j <= numberOfLetters; j++) {
+            const input = document.createElement("input");
+            inputsContainer.type = "text";
+            inputsContainer.id = `guess-${i}-letter-${j}`;
+            inputsContainer.setAttribute("maxlength", "1");
+            tryDiv.appendChild(input);
+        }
+        // tryDiv.innerHTML = `<span>Try ${i}</span>`;
+        // inputsContainer.setAttribute("maxlength", "1");
+        // inputsContainer.setAttribute("data-try", i);
+        // inputsContainer.setAttribute("data-letter", j);
+        // inputsContainer.setAttribute("autocomplete", "off");
+        inputsContainer.appendChild(tryDiv);
+    }
 
 window.onload = function () {
     generateInput();
