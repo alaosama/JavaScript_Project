f82cdfec91d6252a4c9b1da5e115ac4dabc00cb8
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2024-01-09 19:24:21 +0100

    Update js.js
    Object Of Words + Categories

--- My-Game/Hangman/js.js
+++ My-Game/Hangman/js.js
@@ -1,3 +1,11 @@
 const letters = "abcdefghijklmnopqrstuvwxyz";
 
 let lettersArray = Array.from(letters);
+
+
+const words = {
+  programming: ["php", "javascript", "go", "scala", "fortran", "r", "mysql", "python"],
+  movies: ["Prestige", "Inception", "Parasite", "Interstellar", "Whiplash", "Memento", "Coco", "Up"],
+  people: ["Albert Einstein", "Hitchcock", "Alexander", "Cleopatra", "Mahatma Ghandi"],
+  countries: ["Syria", "Palestine", "Yemen", "Egypt", "Bahrain", "Qatar"]
+}
