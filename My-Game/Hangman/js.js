99bef193238e5030dc50c2eafd31d3d976f0aa00
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2024-01-10 16:44:22 +0100

    Update js.js
    Add Class On Span ...

--- My-Game/Hangman/js.js
+++ My-Game/Hangman/js.js
@@ -4,6 +4,20 @@ let lettersArray = Array.from(letters);
 
 let lettersContainer = document.querySelector(".letters");
 
+lettersArray.forEach(letter => {
+
+  let span = document.createElement("span");
+
+  let theLetter = document.createTextNode(letter);
+
+  span.appendChild(theLetter);
+
+  span.className = 'letter-box';
+
+  lettersContainer.appendChild(span);
+
+});
+
 const words = {
   programming: ["php", "javascript", "go", "scala", "fortran", "r", "mysql", "python"],
   movies: ["Prestige", "Inception", "Parasite", "Interstellar", "Whiplash", "Memento", "Coco", "Up"],
