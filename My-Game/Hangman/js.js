76f4fbabd5a9fd368d946b2ab8a8a76c13ec0b4c
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2024-01-10 16:44:50 +0100

    Update js.js

--- My-Game/Hangman/js.js
+++ My-Game/Hangman/js.js
@@ -7,15 +7,15 @@ let lettersContainer = document.querySelector(".letters");
 lettersArray.forEach(letter => {
 
   let span = document.createElement("span");
-
+  
   let theLetter = document.createTextNode(letter);
-
+  
   span.appendChild(theLetter);
-
+  
   span.className = 'letter-box';
-
+  
   lettersContainer.appendChild(span);
-
+  
 });
 
 const words = {
