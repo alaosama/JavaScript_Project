49752c3fe5372ee3c284dbe526d6dd04ebb4dcec
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2024-01-15 22:56:14 +0100

    Add function initMap

--- Mini_Project/Google_Maps_API/js.js
+++ Mini_Project/Google_Maps_API/js.js
@@ -1,1 +1,12 @@
+let map;
 
+function initMap() {
+    map = new google.Map(document.getElementById("map"), {
+        center: { lat: 17.50, Ing: 78.88},
+        zoom: 10,
+    });
+
+    new google.maps.Marker({
+        center: { lat: 17.50, Ing: 78.88},
+    })
+}
