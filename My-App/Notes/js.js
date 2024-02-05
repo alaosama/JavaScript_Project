fe79a0535adaa906dce969e23033d639c5a598f8
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2024-02-05 20:49:23 +0100

    popup() function

--- My-App/Notes/js.js
+++ My-App/Notes/js.js
@@ -1,1 +1,16 @@
+function popup() {
 
+    const popupContainer = document.createElement("div");
+
+    popupContainer.innerHTML = `
+    <div id="popupContainer">
+        <h1>New Note</h1>
+        <textarea id="note-text" placeholder="Enter your note..."></textarea>
+        <div id="btn-container">
+            <button id="submitBtn" onclick="createNote()">Create Note</button>
+            <button id="closeBtn" onclick="closePopup()">Close</button>
+        </div>
+    </div>
+    `;
+    document.body.appendChild(popupContainer);
+}
