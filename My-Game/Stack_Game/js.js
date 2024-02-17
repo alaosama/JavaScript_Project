744f9328c3ab8c1f988ef172b8187d596bea50f2
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2024-02-17 23:25:17 +0100

    Update

--- My-Game/Stack_Game/js.js
+++ My-Game/Stack_Game/js.js
@@ -1,3 +1,35 @@
 const scoreElement = document.getElementById("score");
 const instructionsElement = document.getElementById("instructions");
 const resultsElement = document.getElementById("results");
+
+function setRobotPrecision() {
+  robotPrecision = Math.random() * 1 - 0.5;
+}
+
+function init() {
+  autopilot = true;
+  gameEnded = false;
+  lastTime = 0;
+  stack = [];
+  overhangs = [];
+  setRobotPrecision();
+
+  // Initialize CannonJS
+  world = new CANNON.World();
+  world.gravity.set(0, -10, 0); // Gravity pulls things down
+  world.broadphase = new CANNON.NaiveBroadphase();
+  world.solver.iterations = 40;
+
+  // Initialize ThreeJs
+  const aspect = window.innerWidth / window.innerHeight;
+  const width = 10;
+  const height = width / aspect;
+
+  camera = new THREE.OrthographicCamera(
+    width / -2, // left
+    width / 2, // right
+    height / 2, // top
+    height / -2, // bottom
+    0, // near plane
+    100 // far plane
+  );
