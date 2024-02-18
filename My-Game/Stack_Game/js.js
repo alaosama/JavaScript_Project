798eeda3ee1c937521d9b6a8aaca75b3c755bfaf
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2024-02-18 23:16:53 +0100

    Update (function generateBox)

--- My-Game/Stack_Game/js.js
+++ My-Game/Stack_Game/js.js
@@ -85,3 +85,31 @@ function addOverhang(x, z, width, depth) {
   const overhang = generateBox(x, y, z, width, depth, true);
   overhangs.push(overhang);
 }
+
+function generateBox(x, y, z, width, depth, falls) {
+  // ThreeJS
+  const geometry = new THREE.BoxGeometry(width, boxHeight, depth);
+  const color = new THREE.Color(`hsl(${30 + stack.length * 4}, 100%, 50%)`);
+  const material = new THREE.MeshLambertMaterial({ color });
+  const mesh = new THREE.Mesh(geometry, material);
+  mesh.position.set(x, y, z);
+  scene.add(mesh);
+
+  // CannonJS
+  const shape = new CANNON.Box(
+    new CANNON.Vec3(width / 2, boxHeight / 2, depth / 2)
+  );
+  let mass = falls ? 5 : 0; // If it shouldn't fall then setting the mass to zero will keep it stationary
+  mass *= width / originalBoxSize; // Reduce mass proportionately by size
+  mass *= depth / originalBoxSize; // Reduce mass proportionately by size
+  const body = new CANNON.Body({ mass, shape });
+  body.position.set(x, y, z);
+  world.addBody(body);
+
+  return {
+    threejs: mesh,
+    cannonjs: body,
+    width,
+    depth
+  };
+}
