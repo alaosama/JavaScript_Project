7771709930dce6cc2bef0b76377e6f1d6aa2d155
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2024-02-20 18:23:08 +0100

    Update (const)

--- My-App/Calculator/js.js
+++ My-App/Calculator/js.js
@@ -1,1 +1,21 @@
+const output = document.getElementById("output");
+const form = document.getElementById("calc_form");
+const operand_btns = document.querySelectorAll("button[data-type=operand]");
+
+let is_operator = false;
+operand_btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        remove_active();
+        if(output.value == "0") {
+            output.value = e.target.value;
+        } else if(is_operator) {
+            is_operator = false;
+            output.value = e.target.value;
+        } else if(output.value.includes(".")) {
+            output.value = output.value + "" + e.target.value.replace(".", "");
+        } else {
+            output.value = output.value + "" + e.target.value;
+        }
+    });
+});
 
