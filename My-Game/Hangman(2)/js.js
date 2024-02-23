c0c544f7597d992487261cc2432d5fea34444326
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2024-02-23 22:20:21 +0100

    Update (Display option buttons)

--- My-Game/Hangman(2)/js.js
+++ My-Game/Hangman(2)/js.js
@@ -26,8 +26,29 @@ let options = {
   ],
 };
 
-//count
 let winCount = 0;
 let count = 0;
 
 let chosenWord = "";
+
+const displayOptions = () => {
+  optionsContainer.innerHTML += `<h3>Please Select An Option</h3>`;
+  let buttonCon = document.createElement("div");
+  for (let value in options) {
+    buttonCon.innerHTML += `<button class="options" onclick="generateWord('${value}')">${value}</button>`;
+  }
+  optionsContainer.appendChild(buttonCon);
+};
+
+const blocker = () => {
+  let optionsButtons = document.querySelectorAll(".options");
+  let letterButtons = document.querySelectorAll(".letters");
+  optionsButtons.forEach((button) => {
+    button.disabled = true;
+  });
+
+  letterButtons.forEach((button) => {
+    button.disabled.true;
+  });
+  newGameContainer.classList.remove("hide");
+};
