2c2dde3da67c1ac0eb5104620450201a5db5023e
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2024-02-23 22:19:15 +0100

    Update

--- My-Game/Hangman(2)/js.js
+++ My-Game/Hangman(2)/js.js
@@ -1,4 +1,3 @@
-//Initial References
 const letterContainer = document.getElementById("letter-container");
 const optionsContainer = document.getElementById("options-container");
 const userInputSection = document.getElementById("user-input-section");
@@ -6,3 +5,29 @@ const newGameContainer = document.getElementById("new-game-container");
 const newGameButton = document.getElementById("new-game-button");
 const canvas = document.getElementById("canvas");
 const resultText = document.getElementById("result-text");
+
+let options = {
+  fruits: [
+    "Apple",
+    "Blueberry",
+    "Mandarin",
+    "Pineapple",
+    "Pomegranate",
+    "Watermelon",
+  ],
+  animals: ["Hedgehog", "Rhinoceros", "Squirrel", "Panther", "Walrus", "Zebra"],
+  countries: [
+    "India",
+    "Hungary",
+    "Kyrgyzstan",
+    "Switzerland",
+    "Zimbabwe",
+    "Dominica",
+  ],
+};
+
+//count
+let winCount = 0;
+let count = 0;
+
+let chosenWord = "";
