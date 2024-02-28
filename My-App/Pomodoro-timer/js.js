5f55294a86ef685307eaa0b7f687fe8e2271475b
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2024-02-28 20:32:34 +0100

    Update

--- My-App/Pomodoro-timer/js.js
+++ My-App/Pomodoro-timer/js.js
@@ -1,1 +1,4 @@
-
+let focusButton = document.getElementById("focus");
+let buttons = document.getElementById(".btn");
+let shortBreakButton = document.getElementById("shortbreak");
+let longBreakButton = document.getElementById("longbreak");
