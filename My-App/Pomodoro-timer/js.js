7b60dbe2039a834a21adea4dbe7dde5c4360fce4
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2024-02-29 21:53:35 +0100

    Update

--- My-App/Pomodoro-timer/js.js
+++ My-App/Pomodoro-timer/js.js
@@ -2,3 +2,13 @@ let focusButton = document.getElementById("focus");
 let buttons = document.getElementById(".btn");
 let shortBreakButton = document.getElementById("shortbreak");
 let longBreakButton = document.getElementById("longbreak");
+
+let startBtn = document.getElementById("btn-start");
+let reset = document.getElementById("btn-reset");
+let pause = document.getElementById("btn-pause");
+
+let time = document.getElementById("time");
+let set;
+let count = 59;
+let paused= true;
+let moinCount = 24;
