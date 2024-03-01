5c44739d40226597f7b54f6c9a82b18b56ac776f
Osama ALAHYANE <144958643+alaosama@users.noreply.github.com>
2024-03-01 20:56:36 +0100

    Update (add all let / const)

--- My-App/To-Do-List(2)/js.js
+++ My-App/To-Do-List(2)/js.js
@@ -1,1 +1,6 @@
-
+let todo = JSON.parse(localStorage.getItem("todo")) || [];
+const todoInput = document.getElementById("todoInput");
+const todoList = document.getElementById("todoList");
+const todoCount = document.getElementById("todoCount");
+const addButton = document.querySelector(".btn");
+const deleteButton = document.getElementById("deleteButton");
