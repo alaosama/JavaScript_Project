e1b16f956d8c9cfb5763fbb9ff36f444e45c2386
Osama <144958643+alaosama@users.noreply.github.com>
2024-03-14 18:30:52 +0100

    Update

--- Mini_Project/Res-Card-Slider/js.js
+++ Mini_Project/Res-Card-Slider/js.js
@@ -1,1 +1,16 @@
+let swiperCards = new Swiper('.card__content', {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
 
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
