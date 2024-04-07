58e7714a6de0c30c02b6dfd71be9c2851651bd90
Osama <144958643+alaosama@users.noreply.github.com>
2024-04-07 11:51:03 +0200

    FeedBack

--- web-site_project/FeedBack/js.js
+++ web-site_project/FeedBack/js.js
@@ -2,5 +2,10 @@ const ratings = document.querySelectorAll(".rating");
 const ratingsContainer = document.querySelector(".ratings-container");
 const sendBtn = document.querySelector("#send");
 const panel = document.querySelector("#panel");
-
 let selectedRating = "Satisfied";
+
+function removeActives() {
+  for (let i = 0; i < ratings.length; i++) {
+    ratings[i].classList.remove("active");
+  }
+}
