81783d6d057d529400356031e56079c26e3dfa17
Osama <144958643+alaosama@users.noreply.github.com>
2024-04-11 12:40:19 +0200

    Radio Buttons

--- web-site_project/RadioButtons/js.js
+++ web-site_project/RadioButtons/js.js
@@ -1,1 +1,20 @@
+window.addEventListener ("DOMContentLoaded", () => {
+    const c = new DroppingRadioButtons ("form");
+});
 
+class Dropp {
+    optionID = 0;
+    optionAAnim = null;
+    optionBAnim = null;
+    constructor(el) {
+        this.el = document.querySelector(el);
+        this.el.addEventListener("change", this.updateOption.bind(this));
+    }
+    updateOption(e) {
+        const nextOption = +e.target.getAttribute("data-option");
+        if (this.optionID !== null) {
+            this.animateOption(this.optionID, nextOption);
+        }
+        this.optionID = nextOption;
+    }
+}
