ee3ce35b317488fe523201d73bcbaee72b46cbd5
Osama <144958643+alaosama@users.noreply.github.com>
2024-04-12 15:03:55 +0200

    Confetti Button Animation

--- web-site_project/Effect/Buttons/3-ConfettiButton/js.js
+++ web-site_project/Effect/Buttons/3-ConfettiButton/js.js
@@ -1,1 +1,21 @@
+const confettiBtn = document.querySelector(".confetti-button");
 
+confettiBtn.addEventListener("click", () => { 
+    const canvas = document.createELement("canvas");
+    const container = document.querySelector(".wrapper");
+
+    // set dimensions for canvas
+
+    canvas.width = 600;
+    canvas.height = 600;
+
+    container.appendChild(canvas);
+
+    // create confetti inside the canvas
+
+    const confetti_btn = confetti.create(canvas);
+
+    // remove the canvas after spreading confetti
+
+    confetti_btn().then(() => canvas.remove());
+});
