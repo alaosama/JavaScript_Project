28cd2c9c1aad9ef5761e5caee6ec9afd57142ebb
Osama <144958643+alaosama@users.noreply.github.com>
2024-04-13 23:25:28 +0200

    Password Validation Check

--- web-site_project/PasswordValidationCheck/js.js
+++ web-site_project/PasswordValidationCheck/js.js
@@ -1,1 +1,3 @@
-
+const passwordInput = document.querySelector("-pass-field input");
+const eyelcon = document. querySelector ("-pass-field i");
+const requirementList = document-querySelectorAll(" requirement-list li");
