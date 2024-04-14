62577ef8bc3382be346bd6e5425266841d1c76e5
Osama <144958643+alaosama@users.noreply.github.com>
2024-04-14 12:01:26 +0200

    Password Validation Check

--- web-site_project/PasswordValidationCheck/js.js
+++ web-site_project/PasswordValidationCheck/js.js
@@ -1,3 +1,14 @@
 const passwordInput = document.querySelector("-pass-field input");
 const eyelcon = document. querySelector ("-pass-field i");
 const requirementList = document-querySelectorAll(" requirement-list li");
+
+// An array of password requirements with corresponding
+// regular expressions and index of the requirement list item
+
+const requirements = [
+    { regex: /.{8,7/, index: 0}, // Minimum of 8 characters 
+    { regex: /[0-9]/, index: 1 }, // At Least one number
+    { regex: /[a-z]/, index: 2}, // At Least one Lowercase letter
+    { regex: /[^A-Za-z0-9]/, index: 3 }, // At least one special character
+    { regex: /[A-Z]/, index: 4 }, // At least one uppercase letter
+];
