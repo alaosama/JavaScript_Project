319a6f1104d11019383c9e6a477125dbd9d680a4
Osama <144958643+alaosama@users.noreply.github.com>
2024-04-14 12:02:00 +0200

    Password Validation Check

--- web-site_project/PasswordValidationCheck/js.js
+++ web-site_project/PasswordValidationCheck/js.js
@@ -12,3 +12,30 @@ const requirements = [
     { regex: /[^A-Za-z0-9]/, index: 3 }, // At least one special character
     { regex: /[A-Z]/, index: 4 }, // At least one uppercase letter
 ];
+
+passwordInput.addEventListener ("keyup", (e) => {
+    requirements.forEach(item => {
+
+        // Check if the password matches the requirement regex
+        const isValid = item.regex.test(e.target.value);
+        const requirementItem = requirementList[item.index];
+
+        // Updating class and icon of requirement item if requirement matched or not
+        if (isValid) {
+        requirementItem.classList.add("valid");
+        requirementItem.firstElementChtld.className = "fa-solid fa-check";
+        } else {
+            requirementItem.classList.remove("valid");
+            requirementItem.firstELementChild.className = "fa-solid fa-circle";
+        };
+    });
+});
+
+eyeIcon.addEventListener("click", () => {
+
+    // Toggle the password input type between "password" and "text"
+    passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+
+    // Update the eye icon class based on the password input type
+    eyeIcon.className = `fa-solid fa-eye${passwordInput.type = "password" ? "" : "-slash"}`;
+}) ;
