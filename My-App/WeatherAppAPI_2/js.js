2cb7a341e3a310cde7f2cbdd1171f8755e42bba5
Osama <144958643+alaosama@users.noreply.github.com>
2024-04-15 16:57:32 +0200

    Weather App with API

--- My-App/WeatherAppAPI_2/js.js
+++ My-App/WeatherAppAPI_2/js.js
@@ -29,3 +29,42 @@ document.querySelector("weather__search").addEventListener('submit', e => {
     // clear form
     search.value = "";
 })
+
+// units
+document.querySelector(".weather_unit_celsius").addEventListener('click', () => {
+    if(units !== "metric") {
+        // Change to metric
+        units = "metric"
+
+        // get weather forecast
+        getWeather()
+    }
+})
+document.querySelector(".weather_unit_farenheit").addEventListener('click', () => {
+    if(units !== "imperial"){
+        // Change to imperial
+        units = "imperial";
+
+        // Get weather forecast
+        getWeather()
+    }
+})
+
+function convertTimeStamp(timestamp, timezone){
+    const convertTimezone = timezone / 3600; // Convert Seconds To Hours
+
+    const date = new Date(timestamp * 1000);
+
+    const options = {
+        weekday: "Long",
+        day: "humeric",
+        month: "Long",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        timeEone: `Etc/GMT${convertTimezone >= 0 ? "-" : "+"}$(Math.abs(convertTimezone))`,
+        hour12: true,
+    }
+    
+    return date.toLocaleDateString("en-US", options)
+}
