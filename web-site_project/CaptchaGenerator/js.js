f90d8ef752bb0ab935ec95fceb982a630a58ae33
Osama <144958643+alaosama@users.noreply.github.com>
2024-04-17 00:04:52 +0200

    Captcha Generator

--- web-site_project/CaptchaGenerator/js.js
+++ web-site_project/CaptchaGenerator/js.js
@@ -1,1 +1,15 @@
+// Selecting necessary DOM elements
 
+const captchaTextBox = document.querySelector(".captch_box input");
+
+const refreshButton = document.querySelector(".refresh_button");
+
+const captchaInputBox = document.querySelector(".captch_input input");
+
+const message = document.querySelector(".message");
+
+const submitButton = document.querySelector(".button");
+
+// Variable to store generated captcha
+
+let captchaText = null;
