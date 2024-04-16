f993b0508720cde80c584237d0f5d2a0661c2728
Osama <144958643+alaosama@users.noreply.github.com>
2024-04-17 00:05:11 +0200

    Captcha Generator

--- web-site_project/CaptchaGenerator/js.js
+++ web-site_project/CaptchaGenerator/js.js
@@ -13,3 +13,20 @@ const submitButton = document.querySelector(".button");
 // Variable to store generated captcha
 
 let captchaText = null;
+
+// Function To Generate Captcha
+
+const generateCaptcha = () => {
+    const randomString = Math.random().toString(36).substring(2, 7);
+    const randomStringArray = randomString.split("");
+    const changeString = randomStringArray.map((char) => (Math. random() > 0.5 ? char.toUpperCase() : char));
+    captchaText = changeString.join("");
+    captchaTextBox.value = captchaText;
+    console.log(captchaText);
+}
+
+const refreshBtnClick = () => {
+    generateCaptcha();
+    captchaInputBox.value = "";
+    captchaKeyUpValidate();
+}
