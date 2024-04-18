73d573c5d7dea54415f08027ac311a153e44777a
Osama <144958643+alaosama@users.noreply.github.com>
2024-04-18 20:27:24 +0200

    EMI Calculator

--- My-App/EMI_Calculator/js.js
+++ My-App/EMI_Calculator/js.js
@@ -1,1 +1,10 @@
+const LoanAmount = document.getElementById("loan_amount");
+const loanTenure = document.getElementById("loan_tenure");
+const loanRate = document.getElementById("loan_interest");
 
+const loanEmi = document.querySelector(".loan_emi");
+const loanPrinciple = document.querySelector(".loan_principle");
+const loanTotal = document.querySelector(".loan_total");
+const loanInterest = document.querySelector(".loan_interest_rate");
+
+const submitBtn = document.querySelector(".calculator-btn");
