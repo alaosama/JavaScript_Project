37721d9bf073e247666752e46c14f5591c77a543
Osama <144958643+alaosama@users.noreply.github.com>
2024-04-19 13:36:56 +0200

    EMI Calculator

--- My-App/EMI_Calculator/js.js
+++ My-App/EMI_Calculator/js.js
@@ -8,3 +8,40 @@ const loanTotal = document.querySelector(".loan_total");
 const loanInterest = document.querySelector(".loan_interest_rate");
 
 const submitBtn = document.querySelector(".calculator-btn");
+
+submitBtn.addEventListener("click", function () {
+    const amount = LoanAmount.value;
+    const tenure = loanTenure.value * 12; // 1Year = 12 months
+    const rate = loanRate.value / 12 / 100; // loan rate per year / 100 - loan percentage
+
+    const emi = (amount * rate * (1 + rate) ** tenure) / ((1 + rate) ** tenure - 1);
+    const total = emi * tenure; // total amount to be paid including interest
+    const interest = total - amount; // interest = total amount - principle amount 
+
+    loanEmi.innerHTML = Math.floor(emi);
+    loanPrinciple.innerHTML = Math.floor(amount);
+    loanTotal.innerHTML = Math.floor(total);
+    loanInterest.innerHTML = Math.floor(interest);
+
+    // Loan Chart
+    const xValues = ["Principle", "Interest"];
+    const yValues = [amount, Math.floor(interest)];
+
+    const barColors = ["#4961251", "#000000"];
+
+    new Chart("loanChart", {
+        type: "pie",
+        data: {
+            labels: xValues,
+            datasets: [{
+                backgroundColor: barColors,
+                data: yValues
+            }]
+        },
+        options: {
+            title: {
+                display: false
+            }
+        }
+    });
+});
