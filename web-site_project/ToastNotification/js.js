dc553ea3423b5f8f6e4e5ce2dd28819c024d79c1
Osama <144958643+alaosama@users.noreply.github.com>
2024-04-19 13:47:30 +0200

    Toast Notification

--- web-site_project/ToastNotification/js.js
+++ web-site_project/ToastNotification/js.js
@@ -1,1 +1,23 @@
-
+function showToast(message, duration) {
+    var toast = document.getElementById("toast");
+    var progressBar = document.getElementById("toast-progress");
+    var toastMessage = document.getElementById("toast-message");
+  
+    toastMessage.innerHTML = message;
+    toast.style.display = "block";
+    progressBar.style.width = "0";
+  
+    var width = 0;
+    var interval = setInterval(function() {
+      width += 100 / duration;
+      progressBar.style.width = width + "%";
+      if (width >= 100) {
+        clearInterval(interval);
+        toast.style.animation = "slideOut 0.5s forwards";
+        setTimeout(function() {
+          toast.style.display = "none";
+          toast.style.animation = "";
+        }, 500);
+      }
+    }, duration * 10);
+}
