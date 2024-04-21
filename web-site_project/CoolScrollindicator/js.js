50054adab45b134c95caa5ad80e0849847d0b92b
Osama <144958643+alaosama@users.noreply.github.com>
2024-04-21 23:20:54 +0200

    Cool Scroll Indicator

--- web-site_project/CoolScrollindicator/js.js
+++ web-site_project/CoolScrollindicator/js.js
@@ -1,1 +1,8 @@
+const progressBarEl = document.getElementById("progress-bar");
 
+window.addEventListener("scroll", () => {
+    let heigth = document.body.scrollHeigth - window.innerHeight;
+    let scrollPosition = document.documentElement.scrollTop;
+    let width = (scrollPosition / heigth) * 100;
+    progressBarEl.style.width = `${width}%`;
+});
