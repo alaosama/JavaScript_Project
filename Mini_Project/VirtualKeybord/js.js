2fe9befc518297f641c5e5fa97fa3c77b00b789f
Osama <144958643+alaosama@users.noreply.github.com>
2024-04-23 22:18:22 +0200

    Virtual Keybord

--- Mini_Project/VirtualKeybord/js.js
+++ Mini_Project/VirtualKeybord/js.js
@@ -1,1 +1,8 @@
+const buttons = document.querySelectorAll('.btn');
+const texterea = document.querySelector('textarea');
 
+const delete_btn = document.querySelector('.delete');
+const shift_btn = document.querySelector('.shift');
+const space_btn = document.querySelector('.space');
+
+let chars = [];
