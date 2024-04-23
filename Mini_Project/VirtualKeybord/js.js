8ca2f09deb8d3bc2cb36434b174f154deb56897a
Osama <144958643+alaosama@users.noreply.github.com>
2024-04-23 22:18:43 +0200

    Virtual Keybord

--- Mini_Project/VirtualKeybord/js.js
+++ Mini_Project/VirtualKeybord/js.js
@@ -6,3 +6,27 @@ const shift_btn = document.querySelector('.shift');
 const space_btn = document.querySelector('.space');
 
 let chars = [];
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        texterea.value += btn.innerText
+        chars = texterea.value.split('')
+        // console.log(chars);
+    })
+})
+
+delete_btn.addEventListener('click', () => {
+    chars.pop()
+    texterea.value = chars.join('');
+})
+
+shift_btn.addEventListener('click', () => {
+    buttons.forEach(btn => {
+        btn.classList.toggle('upper')
+    })
+})
+
+space_btn.addEventListener('click', () => {
+    chars.push(' ');
+    texterea.value = chars.join('');
+})
