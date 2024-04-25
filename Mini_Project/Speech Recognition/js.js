9e8e1f67c9d0abfd7bb25a27c11548080fa36129
Osama <144958643+alaosama@users.noreply.github.com>
2024-04-25 22:07:11 +0200

    Speech Recognition

--- Mini_Project/Speech Recognition/js.js
+++ Mini_Project/Speech Recognition/js.js
@@ -3,3 +3,10 @@ let speech = new SpeechSynthesisUtterance();
 let voices = [];
 
 let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voices = voices[0];
+
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+};
