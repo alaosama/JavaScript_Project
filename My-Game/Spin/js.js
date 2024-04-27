0ef31599979424e5fc018fdb24fff322e4a32536
Osama <144958643+alaosama@users.noreply.github.com>
2024-04-27 23:56:54 +0200

    Spin

--- My-Game/Spin/js.js
+++ My-Game/Spin/js.js
@@ -1,8 +1,8 @@
-const whell = document.getElementById("wheel");
-const spinBtn = document.getElementById("spin-btn");
-const finalValue = document.getElementById("final-");
+// const whell = document.getElementById("wheel");
+// const spinBtn = document.getElementById("spin-btn");
+// const finalValue = document.getElementById("final-");
 
-const rotationValues = [
-    { minDegree: 0, maxDegree: 30, value: 2},
-    { minDegree: 31, maxDegree: 30, value: 2},
-];
+// const rotationValues = [
+//     { minDegree: 0, maxDegree: 30, value: 2},
+//     { minDegree: 31, maxDegree: 30, value: 2},
+// ];
