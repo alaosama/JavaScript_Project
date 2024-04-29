3ca60b3a5cb86e8414b96baf9e5f3a607b41544c
Osama <144958643+alaosama@users.noreply.github.com>
2024-04-29 23:00:34 +0200

    Multi-Step From Wizard

--- web-site_project/Multi-Step From Wizard/js.js
+++ web-site_project/Multi-Step From Wizard/js.js
@@ -1,1 +1,15 @@
-
+const sidebarSectionInfo = document.querySelector('.sidebar-section-info');
+const descriptionSection = document.querySelector('.description');
+const middleSection = document.querySelector('.middle-section');
+const backStepSection = document.querySelector('#back-step-btn');
+const nextStepButton = document.querySelector('#next-step-btn');
+const inputContainer = document.querySelector('.input-container');
+const plansContainer = document.querySelector('.plans-container');
+const addOnsContainer = document.querySelector('.add-ons-container');
+const finishingUpContainer = document.querySelector('.finishing-up-container');
+const thankyouContainer = document.querySelector('.thankyou-container');
+const sidebarNumberSections = document.querySelectorAll('.sidebar-number');
+const summaryServiceSection = document.querySelector(
+  '.summary-service-section'
+);
+const totalPriceSection = document.querySelector('.total-price-section');
