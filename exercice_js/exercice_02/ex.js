9a8a6fb9c69225294a731c00f3cbf068924c5ee7
Osama ... <144958643+alaosama@users.noreply.github.com>
2024-11-29 22:27:39 +0100

    Update ex.js

--- exercice_js/exercice_02/ex.js
+++ exercice_js/exercice_02/ex.js
@@ -1,1 +1,11 @@
+/*
+Instructions
+Given a number determine whether or not it is valid per the Luhn formula.
 
+The Luhn algorithm is a simple checksum formula used to validate a variety of identification numbers, such as credit card numbers and Canadian Social Insurance Numbers.
+
+The task is to check if a given string is valid.
+
+Validating a Number
+Strings of length 1 or less are not valid. Spaces are allowed in the input, but they should be stripped before checking. All other non-digit characters are disallowed.
+*/
